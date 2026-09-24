1f2cc74e75cef8eec111ab0cd4b593cdd3963c22
morammariaj <morammariaj@gmail.com>
2026-09-24 01:59:30 -0500

    Refresh PWA cache and version assets

--- sw.js
+++ sw.js
@@ -1,1 +1,5 @@
-const CACHE='logisuite-v1';const ASSETS=['./','./index.html','./style.css','./config.js','./app.js','./manifest.webmanifest'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{const cp=r.clone();caches.open(CACHE).then(x=>x.put(e.request,cp));return r}).catch(()=>caches.match('./index.html'))))});
+const CACHE='logisuite-v4';
+const ASSETS=['./','./index.html','./style.css?v=4','./config.js','./app.js?v=4','./manifest.webmanifest','./icon-192.svg','./icon-512.svg','./favicon.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{const cp=r.clone();caches.open(CACHE).then(x=>x.put(e.request,cp)).catch(()=>{});return r}).catch(()=>caches.match('./index.html'))))});
