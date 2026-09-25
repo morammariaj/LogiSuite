197a28d82e2aaec35cd6ee4c1ebc4f93fa031d0b
morammariaj <morammariaj@gmail.com>
2026-09-24 21:05:07 -0500

    Invalidate stale PWA cache after View Quotes fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="logisuite-clean-1";
+const CACHE="logisuite-clean-2";
 self.addEventListener("install",e=>e.waitUntil(self.skipWaiting()));
 self.addEventListener("activate",e=>e.waitUntil(
   caches.keys()
